e8f299053fa32a5cfc69a88c01e71254b667e809
Jamshidbek97 <urologiya@internet.ru>
2025-08-31 04:23:48 +0900

    Feat: develop socket.io connection

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,8 @@ import router from "./router";
 import routerAdmin from "./router-admin";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
+import { Server as SocketIOServer } from "socket.io";
+import http from "http";
 import session from "express-session";
 import { AUTH_TIMER, MORGAN_FORMAT } from "./libs/config";
 import ConnectMongoDB from "connect-mongodb-session";
@@ -58,4 +60,20 @@ app.set("views", path.join(__dirname, "views"));
 app.use("/admin", routerAdmin);
 app.use("/", router);
 
-export default app;
+const server = http.createServer(app);
+const io = new SocketIOServer(server, {
+  cors: { origin: true, credentials: true },
+});
+
+let summaryClient = 0;
+io.on("connection", (socket) => {
+  summaryClient++;
+  console.log(`Connection & total [${summaryClient}]`);
+
+  socket.on("disconnect", () => {
+    summaryClient--;
+    console.log(`disconnection & total [${summaryClient}]`);
+  });
+});
+
+export default server;
